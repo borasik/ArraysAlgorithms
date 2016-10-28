3b9d0b612ab1ada679ee99ded7d18b6213016d9e
Alex Borsuk <alex.borsuk@dh.com>
2016-10-28 16:12:35 -0400

    Adding printing all possible somearrays of array

--- ArraysAlgorithms/Program.cs
+++ ArraysAlgorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,29 @@ namespace ArraysAlgorithms
     {
         static void Main(string[] args)
         {
+            int[] originalList = new int[] { 1, 2, 3,4 };
+            Stack<int> currentList = new Stack<int>();
+            List<int[]> listOfSubsets = new List<int[]>();
+
+            BuildListOfSubsets(originalList, listOfSubsets, 0, 0, currentList);
+        }
+
+        private static void BuildListOfSubsets(int[] originalList, List<int[]> listOfSubsets, int sizeOfSubsetList, int currentLevel, Stack<int> currentList)
+        {
+
+            foreach (var v in currentList)
+            {
+                Console.Write(v + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int ix = currentLevel; ix < originalList.Length; ix++)
+            {
+                currentList.Push(originalList[ix]);
+                BuildListOfSubsets(originalList, listOfSubsets, originalList.Length - ix + 1, ix + 1, currentList);
+                currentList.Pop();
+            }
         }
     }
 }
