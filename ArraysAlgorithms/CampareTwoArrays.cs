30fcb7d1189c7adba7066614acf201dad52e129e
Alex Borsuk <alex.borsuk@dh.com>
2016-10-30 13:37:17 -0400

    Finished find all max same substrings of two arrays

---
+++ ArraysAlgorithms/CampareTwoArrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAlgorithms
+{
+    public static class CampareTwoArrays
+    {
+        public static bool CompairTwoCharArrays(this char[] list, char[] compairTo)
+        {
+            if (list.Length != compairTo.Length) return false;
+
+            var i = 0;            
+            while(i < list.Count())
+            {
+                if (list[i] != compairTo[i])
+                    return false;
+                i++;
+            }
+
+            return true;
+        }
+    }
+}
--- ArraysAlgorithms/Program.cs
+++ ArraysAlgorithms/Program.cs
@@ -11,24 +11,33 @@ namespace ArraysAlgorithms
     {
         static void Main(string[] args)
         {
-            char[] originalList = new char[] { 'A', 'B', 'C', 'D'};
+            char[] originalList = new char[] { 'H', 'A', 'R', 'R', 'Y'};
             Stack<char> currentList = new Stack<char>();
             List<char[]> listOfSubsets = new List<char[]>();
 
-            char[] originalList2 = new char[] { 'A', 'B', 'D', 'C'};
+            char[] originalList2 = new char[] { 'S', 'A', 'L', 'L', 'Y'};
             Stack<char> currentList2 = new Stack<char>();
             List<char[]> listOfSubsets2 = new List<char[]>();
 
             var resultSubList = new List<char>();
             var resultSubList2 = new List<char>();
 
+            Console.WriteLine("Children of String A:");
             var result = BuildListOfSubsets(originalList, listOfSubsets, 0, currentList);
+          
+
+            Console.WriteLine("Children of String B:");
             var  result2 = BuildListOfSubsets(originalList2, listOfSubsets2, 0, currentList);
 
             var equalStrings = FindEqualStrings(result, result2);
 
-            var minStrings = FindMinStrings();
+            var minStrings = FindMinStrings(equalStrings);
 
+            Console.WriteLine("Max Shared Sub String: ");
+            for(var c = minStrings.Count()-1; c>=0;c--)
+            {
+                Console.Write(minStrings[c]);
+            }
         }
 
         private static List<char[]> BuildListOfSubsets(char[] originalList, List<char[]> listOfSubsets, int currentLevel, Stack<char> currentList)
@@ -63,7 +72,7 @@ namespace ArraysAlgorithms
             {
                 for(var j = 0; j < result2.Count; j++)
                 {
-                    if(result[i] == result2[j])
+                    if(result[i].CompairTwoCharArrays(result2[j]))
                     {
                         equalStrings.Add(result[i]);
                         break;
@@ -74,10 +83,21 @@ namespace ArraysAlgorithms
             return equalStrings;
         }
 
-        private static char[] FindMinStrings()
+        private static char[] FindMinStrings(List<char[]> charArray)
         {
-            //Find Min
-            return new char[] { };
+            var maxIndex = -1;
+            var maxLength = 0;
+
+            for(var c = 0; c<charArray.Count; c++)
+            {
+                if (charArray[c].Count() > maxLength)
+                {
+                    maxIndex = c;
+                    maxLength = charArray[c].Count();
+                }                    
+            }
+
+            return maxIndex != -1 ? charArray[maxIndex] : new char[0];
         }
     }
 }
