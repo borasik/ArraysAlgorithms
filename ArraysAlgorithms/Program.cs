22afc2e1e21954bd5ba008ac8afc62ab8ec425b8
Alex Borsuk <alex.borsuk@dh.com>
2016-10-30 12:34:34 -0400

    Find Min Sub String of two arrays

--- ArraysAlgorithms/Program.cs
+++ ArraysAlgorithms/Program.cs
@@ -11,19 +11,37 @@ namespace ArraysAlgorithms
     {
         static void Main(string[] args)
         {
-            int[] originalList = new int[] { 1, 2, 3,4 };
-            Stack<int> currentList = new Stack<int>();
-            List<int[]> listOfSubsets = new List<int[]>();
+            char[] originalList = new char[] { 'A', 'B', 'C', 'D'};
+            Stack<char> currentList = new Stack<char>();
+            List<char[]> listOfSubsets = new List<char[]>();
 
-            BuildListOfSubsets(originalList, listOfSubsets, 0, 0, currentList);
+            char[] originalList2 = new char[] { 'A', 'B', 'D', 'C'};
+            Stack<char> currentList2 = new Stack<char>();
+            List<char[]> listOfSubsets2 = new List<char[]>();
+
+            var resultSubList = new List<char>();
+            var resultSubList2 = new List<char>();
+
+            var result = BuildListOfSubsets(originalList, listOfSubsets, 0, currentList);
+            var  result2 = BuildListOfSubsets(originalList2, listOfSubsets2, 0, currentList);
+
+            var equalStrings = FindEqualStrings(result, result2);
+
+            var minStrings = FindMinStrings();
+
         }
 
-        private static void BuildListOfSubsets(int[] originalList, List<int[]> listOfSubsets, int sizeOfSubsetList, int currentLevel, Stack<int> currentList)
+        private static List<char[]> BuildListOfSubsets(char[] originalList, List<char[]> listOfSubsets, int currentLevel, Stack<char> currentList)
         {
-
+            var resultSubList = new List<char>();
             foreach (var v in currentList)
             {
                 Console.Write(v + " ");
+                resultSubList.Add(v);
+            }
+            if(resultSubList.Count > 0)
+            {
+                listOfSubsets.Add(resultSubList.ToArray<char>());
             }
 
             Console.WriteLine();
@@ -31,9 +49,35 @@ namespace ArraysAlgorithms
             for (int ix = currentLevel; ix < originalList.Length; ix++)
             {
                 currentList.Push(originalList[ix]);
-                BuildListOfSubsets(originalList, listOfSubsets, originalList.Length - ix + 1, ix + 1, currentList);
+                BuildListOfSubsets(originalList, listOfSubsets, ix + 1, currentList);
                 currentList.Pop();
             }
+
+            return listOfSubsets;
+        }
+
+        private static List<char[]> FindEqualStrings(List<char[]> result, List<char[]> result2)
+        {
+            var equalStrings = new List<char[]>();
+            for(var i=0; i<result.Count; i++)
+            {
+                for(var j = 0; j < result2.Count; j++)
+                {
+                    if(result[i] == result2[j])
+                    {
+                        equalStrings.Add(result[i]);
+                        break;
+                    }
+                }
+            }
+
+            return equalStrings;
+        }
+
+        private char[] FindMinStrings()
+        {
+            //Find Min
+            return new char[];
         }
     }
 }
